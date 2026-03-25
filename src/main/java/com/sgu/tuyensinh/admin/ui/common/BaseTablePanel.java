959f607dd48b263239a51f865e5f91e23ab672b8
tvthuong290605 <tvthuong290605@uong290605gmail.90605gmailcom>
2026-03-25 11:15:13 +0700

    update code

--- src/main/java/com/sgu/tuyensinh/admin/ui/common/BaseTablePanel.java
+++ src/main/java/com/sgu/tuyensinh/admin/ui/common/BaseTablePanel.java
@@ -1,3 +1,5 @@
+package com.sgu.tuyensinh.admin.ui.common;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -9,21 +11,17 @@ public class BaseTablePanel extends JPanel {
     public BaseTablePanel(String[] columnNames) {
         setLayout(new BorderLayout());
 
-        // Khởi tạo model với tên cột
         tableModel = new DefaultTableModel(columnNames, 0);
         table = new JTable(tableModel);
 
-        // Thêm JScrollPane để cuộn
         JScrollPane scrollPane = new JScrollPane(table);
         add(scrollPane, BorderLayout.CENTER);
     }
 
-    // Hàm thêm một dòng dữ liệu
     public void addRow(Object[] rowData) {
         tableModel.addRow(rowData);
     }
 
-    // Hàm lấy JTable (nếu cần thao tác thêm)
     public JTable getTable() {
         return table;
     }
--- src/main/java/com/sgu/tuyensinh/admin/ui/common/MessageDialog.java
+++ src/main/java/com/sgu/tuyensinh/admin/ui/common/MessageDialog.java
@@ -1,3 +1,5 @@
+package com.sgu.tuyensinh.admin.ui.common;
+
 import javax.swing.*;
 
 public class MessageDialog {
